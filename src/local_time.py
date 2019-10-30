534e990d3f2f445dd85f3d65dc4537b0147018e4
PG-Payne <Geoffrey.Payne@city.ac.uk>
2019-10-30 16:54:01 +0000

    getting used to it

--- src/local_time.py
+++ src/local_time.py
@@ -2,4 +2,4 @@ import time
 
 class LocalTime:
   def __init__(self):
-    self.localtime = time.asctime( time.localtime(time.time()) )
+    self.localtime = time.asctime( time.localtime(time.time()))
