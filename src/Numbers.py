9b5b2d089d667da5bf65d612cc72d270ef702657
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2019-12-23 14:45:06 +0000

    REmove repeat code

--- src/Numbers.py
+++ src/Numbers.py
@@ -24,9 +24,6 @@ train_labels = data[2]
 test_labels = data[3]
 train_labels_one_hot = data[4]
 test_labels_one_hot = data[5]
-image_size = 28 # width and length
-no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
-image_pixels = image_size * image_size
 
 lr = np.arange(no_of_different_labels)
 # transform labels into one hot representation
